0663e1efe54bb62ab8e71f8bf18c84691b3eac95
Johannes Breuer <johannes.breuer@cais-research.de>
2026-02-02 12:36:55 +0100

    add notebook and Binder files

---
+++ binder/install.R
@@ -0,0 +1,6 @@
+install.packages(c("dplyr",
+                   "readr",
+                   "ggplot2",
+                   "quanteda",
+                   "quanteda.textplots",
+                   "quanteda.textstats"))
